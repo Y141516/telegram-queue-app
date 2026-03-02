a5d6f3a4178d343d139a621a841f739b1e0ba479
Y141516 <vaktgoplani9120@gmail.com>
2026-03-03 00:36:46 +0530

    Final 6

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,11 +5,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <script src="https://telegram.org/js/telegram-web-app.js"></script>
       </head>
-      <body>
+      <body suppressHydrationWarning>
         {children}
       </body>
     </html>
